9b18adee5b97c409151b7a4edf87933063df2ac0
Rohan Taneja <rohantaneja63@gmail.com>
2016-11-19 14:59:53 +0530

    cm: makefile fix

--- cm.mk
+++ cm.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/jiayu/s3_h560/device_s3_h560.mk)
+$(call inherit-product, device/lenovo/aio_row/device_aio_row.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -6,6 +6,6 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := aio_row
 PRODUCT_NAME := cm_aio_row
-PRODUCT_BRAND := Lenovo A7000-a
+PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := A7000-a
 PRODUCT_MANUFACTURER := Lenovo
